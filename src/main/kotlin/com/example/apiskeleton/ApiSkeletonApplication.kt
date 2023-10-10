839f5d36f479795672ad331878e8849efccc6890
김용성 <yongsung.kim@onestorecorp.com>
2023-10-11 01:28:20 +0900

    first commit

---
+++ src/main/kotlin/com/example/apiskeleton/ApiSkeletonApplication.kt
@@ -0,0 +1,11 @@
+package com.example.apiskeleton
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiSkeletonApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiSkeletonApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/apiskeleton/ApiSkeletonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.apiskeleton
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiSkeletonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
